8f7f0ffe8726e2150625cad9f010b07048963b98
sajib1066 <sajib1066@gmail.com>
2019-11-19 14:52:41 +0600

    setup job-find page

--- job_section/urls.py
+++ job_section/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import job_post
+from .views import job_post, job_find
 
 urlpatterns = [
-    path('post', job_post, name='job-post')
+    path('post', job_post, name='job-post'),
+    path('find', job_find, name='job-find')
 ]
--- job_section/views.py
+++ job_section/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def job_post(request):
     return render(request, 'job-section/job-post.html')
+
+def job_find(request):
+    return render(request, 'job-section/find-job.html')
