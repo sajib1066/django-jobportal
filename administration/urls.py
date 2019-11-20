353ab73324b97861763b3819684fd7536ce43052
sajib1066 <sajib1066@gmail.com>
2019-11-20 16:17:22 +0600

    setup employee login and register page

--- administration/urls.py
+++ administration/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import candidate_login_page, candidate_register_page
+from .views import candidate_login_page, candidate_register_page, employee_login_page, employee_register_page
 
 urlpatterns = [
     path('candidate/login', candidate_login_page, name='candidate-login'),
-    path('candidate/register', candidate_register_page, name='candidate-register')
+    path('candidate/register', candidate_register_page, name='candidate-register'),
+    path('employee/login', employee_login_page, name='employee-login'),
+    path('employee/register', employee_register_page, name='employee-register')
 ]
--- administration/views.py
+++ administration/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def candidate_login_page(request):
-    return render(request, 'administration/login.html')
+    return render(request, 'administration/candidate-login.html')
 
 def candidate_register_page(request):
-    return render(request, 'administration/register.html')
+    return render(request, 'administration/candidate-register.html')
+
+def employee_login_page(request):
+    return render(request, 'administration/employee-login.html')
+
+def employee_register_page(request):
+    return render(request, 'administration/employee-register.html')
