86adc9ea1274917c66aea7ff9a4f6e5a865a4d16
Sajib Hossain <sajib1066@gmail.com>
2021-01-21 23:28:05 +0600

    setup postgres

--- jobpply/settings.py
+++ jobpply/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'jobpply.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'portal_db',
+        'USER': 'portal_user',
+        'PASSWORD': 'portal_pass',
+        'PORT': 5432,
     }
 }
 
